49e65bbd563680ef018f0ccad2300a3050300d2a
lds970311 <ldk2696612575@gmail.com>
2023-11-26 23:22:40 +0800

    1. 完成商品详情页功能的开发

---
+++ mall_parent/web/web-pc/target/classes/static/js/api/payment.js
@@ -0,0 +1,18 @@
+const payment = {
+
+    api_name: '/api/payment/weixin',
+
+    createNative(orderId) {
+        return request({
+            url: this.api_name + `/createNative/` + orderId,
+            method: 'get'
+        })
+    },
+
+    queryPayStatus(orderId) {
+        return request({
+            url: this.api_name + `/queryPayStatus/` + orderId,
+            method: 'get'
+        })
+    }
+}
